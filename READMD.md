2c453fc34b7a0b5365c39ebe9fa591650e6f076a
gbeea1004 <jvckigh@gmail.com>
2021-07-14 23:14:25 +0900

    chore: 초기 셋팅 및 예제 코드 작성

---
+++ READMD.md
@@ -0,0 +1,10 @@
+# Refactoring 스터디 ✍️
+
+## 목표
+* 리팩토링 기법과 타입스크립트 문법을 익히는 것
+
+## Spec
+* TypeScript
+* Jest
+* ESLint
+* Babel
---
+++ ch01/src/types.ts
@@ -0,0 +1,21 @@
+type PlayID = 'hamlet' | 'as-like' | 'othello';
+type Genre = 'tragedy' | 'comedy';
+
+type Play = {
+  name: string;
+  type: Genre;
+};
+
+export type Plays = {
+  [playID in PlayID]: Play;
+};
+
+export type Invoice = {
+  customer: string;
+  performances: Performance[];
+};
+
+type Performance = {
+  playID: PlayID;
+  audience: number;
+};
